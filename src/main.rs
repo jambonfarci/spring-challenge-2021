9da02a1816194af65767edc80dfa556691772c4d
Maël <mael.fortune@gmail.com>
2021-05-06 11:56:19 +0200

    Setup

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+/*macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}*/
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+
+}
